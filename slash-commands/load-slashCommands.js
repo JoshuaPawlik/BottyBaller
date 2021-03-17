c337853c7705c47a638107a3435ceaf4e3aeda22
Joshua Pawlik <joshuapawlik12@gmail.com>
2021-03-17 11:35:00 -0500

    Seperate slash commands functionality from main.js

---
+++ slash-commands/load-slashCommands.js
@@ -0,0 +1,84 @@
+module.exports = async (client, guildId) => {
+
+    const getApp = (guildId) => {
+        const app = client.api.applications(client.user.id)
+        if (guildId){
+          app.guilds(guildId)
+        }
+        return app;
+      }
+
+    await getApp(guildId).commands.post({
+        data: {
+          name: 'ping',
+          description: 'A simple ping pong command'
+        }
+      })
+    
+    
+      await getApp(guildId).commands.post({
+        data: {
+            name: "confess",
+            description: "Make a private confession",
+            options: [{
+                type: 3,
+                name: "confession",
+                description: "Something",
+                required: true
+    
+            }]
+        }
+    });
+    
+      client.ws.on('INTERACTION_CREATE', async (interaction) => {
+        const { name, options } = interaction.data
+        const command = name.toLowerCase();
+    
+        const args = {};
+    
+        if (options) {
+          for (const option of options){
+            const {name,value} = option
+            args[name] = value
+          }
+        }
+    
+        console.log(args)
+    
+        if (command === 'ping'){
+    
+          reply(interaction, 'pong')
+          console.log("done")
+    
+       } else if (command === 'confess'){
+    
+        const Embed = {
+            color: Math.floor(Math.random()*16777215).toString(16),
+            title: `Anonymous Confession`,
+            description: `"${args.confession}"`,
+            timestamp: new Date(),
+            footer: {
+                text: 'Posted',
+                icon_url: '',
+            },
+        };
+        client.channels.cache.get(`${interaction.channel_id}`).send({embed: Embed})
+        .then(() => {
+          reply(interaction, 'Your confesssion has been anonomously submitted!')
+        })
+        .catch(console.error);
+      }
+    })
+    
+      const reply = (interaction, response) => {
+        client.api.interactions(interaction.id, interaction.token).callback.post({
+            data:{
+              type: 3,
+              data: {
+                content: response,
+                flags: 64
+              }
+            } 
+          })
+      }
+}

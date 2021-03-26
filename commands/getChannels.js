28817011415355f0ff8c88ef60b53ca95b6b9221
Joshua Pawlik <joshuapawlik12@gmail.com>
2021-03-26 13:01:11 -0500

    Add usage of reassignable channels for slash-coms

--- commands/getChannels.js
+++ commands/getChannels.js
@@ -5,7 +5,7 @@ module.exports = {
     minArgs: 1,
     callback: async (message, args, text, client) => {
         const guildId = message.guild.id;
-        const result = await channels.getChannels(guildId)
+        const result = await channels.getChannels(guildId, args[0])
 
         console.log('result =======> ', result)
 

2e660059fb4831ac01299a18f7ef9b944a14973a
Joshua Pawlik <joshuapawlik12@gmail.com>
2021-04-01 17:57:06 -0500

    Delete requiring of config.json

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
 const mongo = require('./mongo')
 const path = require('path')
 const fs = require('fs')

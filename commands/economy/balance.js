ee8fbe1781b57ad749eab372f02e2dcd3a3f9adc
Joshua Pawlik <joshuapawlik12@gmail.com>
2021-04-02 01:13:37 -0500

    Add an embed reply for checking balances

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -13,10 +13,60 @@ module.exports = {
         const userId = target.id;
         
         const coffeebeans = await economy.getCoffeebeans(guildId,userId);
-        if (!args[0]){
-            message.reply(`You have ${coffeebeans} <:coffeebeans:820214111887556638>`)
-        }else {
-            message.reply(`That user has ${coffeebeans} <:coffeebeans:820214111887556638>`)
-        }
+
+
+
+        const Embed = {
+            color: "#8d7070",
+            title: `${target.username}\'s balance:`,
+            // url: 'https://discord.js.org',
+            // author: {
+            //     name: 'Some name',
+            //     icon_url: 'https://i.imgur.com/wSTFkRM.png',
+            //     url: 'https://discord.js.org',
+            // },
+            description: `${coffeebeans}<:coffeebeans:820214111887556638>`,
+            // thumbnail: {
+                // url: 'https://i.imgur.com/wSTFkRM.png',
+                // url: message.author.avatarURL()
+            // },
+            // fields: [
+            //     {
+            //         name: 'Regular field title',
+            //         value: 'Some value here',
+            //     },
+            //     {
+            //         name: '\u200b',
+            //         value: '\u200b',
+            //         inline: false,
+            //     },
+            //     {
+            //         name: 'Inline field title',
+            //         value: 'Some value here',
+            //         inline: true,
+            //     },
+            //     {
+            //         name: 'Inline field title',
+            //         value: 'Some value here',
+            //         inline: true,
+            //     },
+            //     {
+            //         name: 'Inline field title',
+            //         value: 'Some value here',
+            //         inline: true,
+            //     },
+            // ],
+            // image: {
+            //     url: 'https://i.imgur.com/wSTFkRM.png',
+            // },
+            // timestamp: new Date(),
+            // footer: {
+            //     text: 'Asked',
+            //     icon_url: '',
+            // },
+        };
+        // if (!args[0]){
+            message.channel.send({embed: Embed})
+        // }
     }
 }

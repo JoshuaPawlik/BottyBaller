d25057710c0e6ee9567f9e27e2e788bbca71c931
Joshua Pawlik <joshuapawlik12@gmail.com>
2021-04-09 15:56:44 -0500

    Allow users to earn coffeebeans on upvote

---
+++ features/addCoffeeBeansOnUpvote.js
@@ -0,0 +1,28 @@
+const economy = require('../economy')
+const channels = require('../channels')
+let upvoteCache = [];
+const { channelsCache } = channels;
+module.exports = (client) => {
+    client.on('messageReactionAdd', async (reaction, user) => {
+        if (user.bot === true) return;
+        const { message, emoji } = reaction
+        if (emoji.name !== '<:upvote:767990923875319809>' && emoji.name !== '👍') return;
+
+        // console.log("User object ===> ",user)
+        // console.log("Reaction object ===> ",reaction)
+        const messageAuthor = message.author
+        const channelId = message.channel.id
+
+        if (user.id === messageAuthor.id) return; //Do not forget to uncomment this!!!!!!!!!!!!!!
+        if (!channelsCache[message.guild.id]['upDown'].includes(channelId)) return;
+        else if (upvoteCache.includes(`${user.id}-${message.id}`)) return;
+        else {
+            upvoteCache.push(`${user.id}-${message.id}`)
+            // console.log(upvoteCache);
+    
+            const update = economy.addCoffeebeans(messageAuthor.id, 1);
+            console.log(update);
+            return;
+        }
+    })
+}

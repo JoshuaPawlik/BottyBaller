288b143cce16eea4712013d3e18729514272a903
Joshua Pawlik <joshuapawlik12@gmail.com>
2021-05-24 16:22:42 -0500

    add auto Heart react to images/links

---
+++ features/heartSubmissions.js
@@ -0,0 +1,19 @@
+const channels = require('../channels')
+
+module.exports = (client) => {
+    client.on('message', async (message) => {
+        
+        let carryOn = await channels.carryOn(message, 'heartSubmissions')
+        if(!carryOn){
+            return
+        }
+        else if (message.attachments.size > 0){
+            message.react("❤️")
+                 .catch(err => {
+                    console.error(err)
+                })
+            return;
+        }
+    })
+    return
+}

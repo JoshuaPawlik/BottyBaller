87bf06b059ffacf5b4dd680e3031d360f0577bcd
Joshua Pawlik <joshuapawlik12@gmail.com>
2021-06-30 16:49:45 -0500

    Let Caffeinated members claim more coffeebeans

--- commands/economy/redeem-daily.js
+++ commands/economy/redeem-daily.js
@@ -6,27 +6,37 @@ module.exports = {
     callback: async (message, arguments) => {
         const userId = message.author.id;
         const dailyKeyPrefix = 'daily-';
-        // const guildId = message.guild.id;
         const redisClient = await redis();
-        // economy.setClaimedDaily(userId, false);
         let claimedDaily = await economy.getClaimedDailyStatus(userId);
-
-        if (claimedDaily === false){
-            economy.addCoffeebeans(userId, 25).then(() => {
-                economy.setClaimedDaily(userId, true);
-            })
-            redisClient.set(`${dailyKeyPrefix}${userId}`, true,'EX', 86400);
-            message.channel.send("You have claimed your daily 25 <:coffeebeans:820214111887556638>!");
-        } else {
-            message.channel.send("You must wait a full 24 hours before claiming your daily again!");
-        }
-
+        
         redis.expire(message => {
             if (message.startsWith(dailyKeyPrefix)){
                 const split = message.split('-');
                 economy.setClaimedDaily(split[1], false);
             }
-        }) 
-
+        })
+    
+        if (!claimedDaily){
+            try {
+                if (message.member.roles.cache.some(role => role.name === 'Caffeinated')) {
+                    economy.addCoffeebeans(userId, 40).then(() => {
+                        economy.setClaimedDaily(userId, true);
+                        message.channel.send("You have claimed your daily 40 <:coffeebeans:820214111887556638>!");
+                    })
+                } else {
+                    economy.addCoffeebeans(userId, 25).then(() => {
+                        economy.setClaimedDaily(userId, true);
+                        message.channel.send("You have claimed your daily 25 <:coffeebeans:820214111887556638>!");
+                    })
+                }
+                redisClient.set(`${dailyKeyPrefix}${userId}`, true,'EX', 86400 );
+                // redisClient.set(`${dailyKeyPrefix}${userId}`, true,'EX', 10 );
+            } 
+            finally {
+                // redisClient.quit();
+            }
+        } else {
+            message.channel.send("You must wait a full 24 hours before claiming your daily again!");
+        }
     }
 }   

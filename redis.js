d3f20cf7e171c86f18bf9188cb5e3e37da669c88
Joshua Pawlik <joshuapawlik12@gmail.com>
2021-06-30 16:50:56 -0500

    Utilize a single redis client connection

--- redis.js
+++ redis.js
@@ -1,33 +1,49 @@
 const redis = require('redis')
 const redisPath = process.env.REDIS_PATH;
+
+let client = null;
+let sub = null;
+let pub = null;
 module.exports = async () => {
-  return await new Promise((resolve, reject) => {
-    const client = redis.createClient({
-      url: redisPath,
-    })
 
-    client.on('error', (err) => {
-      console.error('Redis error:', err)
-      client.quit()
-      reject(err)
-    })
-
-    client.on('ready', () => {
-      resolve(client)
-    })
-  })
+  if (client === null){
+    console.log("NEW CLIENT")
+    return await new Promise((resolve, reject) => {
+      client = redis.createClient({
+        url: redisPath,
+      })
+  
+      client.on('error', (err) => {
+        console.error('Redis error:', err)
+        client.quit()
+        reject(err)
+      })
+  
+      client.on('ready', () => {
+        console.log("Connected to redis")
+        resolve(client)
+      })
+    }) 
+  } else {
+    console.log("OLD CLIENT")
+    return client;
+  }
 }
 
 module.exports.expire = (callback) => {
   const expired = () => {
-    const sub = redis.createClient({ url: redisPath })
-    sub.subscribe('__keyevent@0__:expired', () => {
-      sub.on('message', (channel, message) => {
-        callback(message)
+    if (sub === null){
+      sub = redis.createClient({ url: redisPath })
+      sub.subscribe('__keyevent@0__:expired', () => {
+        sub.on('message', (channel, message) => {
+          callback(message)
+        })
       })
-    })
+    }
   }
 
-  const pub = redis.createClient({ url: redisPath })
+  if (pub === null){
+    pub = redis.createClient({ url: redisPath })
+  }
   pub.send_command('config', ['set', 'notify-keyspace-events', 'Ex'], expired())
 }
